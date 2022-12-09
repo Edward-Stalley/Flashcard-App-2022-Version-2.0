be0aa5717f12243183d6bc3496df006783083474
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2022-12-09 16:23:59 +0900

    Created Backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# Flashcard App 2022 Version 2.0
- New version of Flashcards using Next.js & MySQL
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("connected to backend");
+});

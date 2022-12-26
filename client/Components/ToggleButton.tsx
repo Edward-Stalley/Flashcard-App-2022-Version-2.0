81754e94623053465b10b96c12bb5cd00a907c31
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2022-12-26 22:14:12 +0900

    Lightbulb SVG for toggle

--- client/Components/ToggleButton.tsx
+++ client/Components/ToggleButton.tsx
@@ -1,22 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import MyThemeContext from "../store/myThemeContext";
+import Lightbulb from "./Lightbulb";
 
 export default function ToggleButton() {
+  const [darkOn, setDarkOn] = useState(false);
+
   // Theme
   const themeCtx: { isDarkMode?: boolean; toggleThemeHandler: () => void } = useContext(MyThemeContext);
 
   function toggleThemeHandler(): void {
     themeCtx.toggleThemeHandler();
+    setDarkOn((prevDarkOn) => !prevDarkOn);
   }
   return (
     <div className="bg-slate-200 dark:bg-bd-1 flex justify-end p-4 ">
-      <button
+      <Lightbulb onClick={toggleThemeHandler} darkOn={darkOn} />
+      {/* <button
         type="button"
         className=" w-32 h-16 bg-blue-200 text-slate-800 dark:bg-zinc-200 dark:text-black rounded   "
         onClick={toggleThemeHandler}
       >
         Toggle Theme
-      </button>
+      </button> */}
     </div>
   );
 }

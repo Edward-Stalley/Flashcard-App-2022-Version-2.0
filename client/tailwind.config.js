959e4ff45c1fe880362a46bbf835881a79402eae
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2022-12-26 11:08:18 +0900

    Update tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  important: true,
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {

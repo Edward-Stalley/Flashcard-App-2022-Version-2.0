3e83b248f784f2fad291d4bb3c6ee994cc467aea
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-01 12:02:47 +0900

    Edit

--- client/pages/api/weeks/[id].ts
+++ client/pages/api/weeks/[id].ts
@@ -1,5 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import mysql from "mysql";
+
+export default function getAllWeeks(req: NextApiRequest, res: NextApiResponse) {
+  res.json({ byID: req.query.id, message: "get class by id" });
+
+  // const q = `SELECT * FROM listening_kiso WHERE week = ${week}`;
+  // db.query(q, (err, data) => {
+  //   if (err) return res.json(err);
+  //   res.json(data);
+  // });
+  // res.json({ week: "all weeks" });
+}
+
+// const week = req.query.id;
+//
+
+// import mysql from "mysql";
 
 // const db = mysql.createConnection({
 //   host: "sql6.freemysqlhosting.net",
@@ -7,15 +22,3 @@ import mysql from "mysql";
 //   password: "beh4prkep6",
 //   database: "sql6587358",
 // });
-
-export default function getAllWeeks(req: NextApiRequest, res: NextApiResponse) {
-  const week = req.query.id;
-
-  // const q = `SELECT * FROM listening_kiso WHERE week = ${week}`;
-  // db.query(q, (err, data) => {
-  //   if (err) return res.json(err);
-  //   res.json(data);
-  // });
-  res.json({ byID: req.query.id, message: "get class by id" });
-  // res.json({ week: "all weeks" });
-}

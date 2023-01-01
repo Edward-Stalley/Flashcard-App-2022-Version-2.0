5b06357165fa95cbd1deea191d80aea584c64d72
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-01 11:37:05 +0900

    Update folder structure

--- client/pages/api/vehicle/[id].ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function getClassById(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "GET") {
-    res.status(500).json({ message: "sorry only GET requests are accepted" });
-  }
-
-  res.json({ hello: "world vehicle", method: req.method });
-}

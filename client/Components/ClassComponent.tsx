2e6d8010e4df2cef8b5af917e2984e000d96ca6d
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-01 12:53:53 +0900

    Update Class Component

--- client/Components/ClassComponent.tsx
+++ client/Components/ClassComponent.tsx
@@ -17,7 +17,7 @@ export default function Class(props: {
   return (
     <Link
       className="hover:bg-blue-300/75 bg-blue-200 dark:hover:bg-zinc-700 dark:bg-bd-1 dark:text-td-1  h-fit w-64 p-5 rounded flex items-center m-1 justify-center"
-      href={`/ClassSelector/${props.year}/${props.week}/${props.class}`}
+      href={`api/ClassSelector/${props.year}/${props.week}/${props.class}`}
     >
       {props.class}
     </Link>

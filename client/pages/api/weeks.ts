6d71b94622b1c654bd2b8a1b0d433b8ce502ec96
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-01 11:54:26 +0900

    Week API

---
+++ client/pages/api/weeks.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql";
+
+const db = mysql.createConnection({
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6587358",
+  password: "beh4prkep6",
+  database: "sql6587358",
+});
+
+export default function getAllWeeks(req: NextApiRequest, res: NextApiResponse) {
+  const week = req.query;
+
+  const q = `SELECT * FROM listening_kiso WHERE week = ${week}`;
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    res.json(data);
+  });
+  // res.json({ week: "all weeks" });
+}

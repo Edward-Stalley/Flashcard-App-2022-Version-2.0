0bfd677011d59024e69840d4a404cdfa99712493
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-02 15:00:42 +0900

    API UPDATE

--- client/Components/ClassComponent.tsx
+++ client/Components/ClassComponent.tsx
@@ -18,7 +18,7 @@ export default function Class(props: {
     <Link
       className=" nav_button bg-but-d  dark:hover:bg-bd-1 dark:bg-but-d dark:text-td-1  h-fit w-64 p-5 rounded-full flex items-center m-1 justify-center"
       // for local host
-      href={`/ClassSelector/${props.year}/${props.week}/${props.class}`}
+      href={`api/ClassSelector/${props.year}/${props.week}/${props.class}`}
       // Use the one below for deployed version
 
       // href={`api/ClassSelector/${props.year}/${props.week}/${props.class}`}

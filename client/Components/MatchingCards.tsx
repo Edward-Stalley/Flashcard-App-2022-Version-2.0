1daa5f345061ae18ea3896e2bfead076747c70e8
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-04 10:53:57 +0900

    CSS

--- client/Components/MatchingCards.tsx
+++ client/Components/MatchingCards.tsx
@@ -134,7 +134,7 @@ export default function MatchingCards(props: {
           data-key-match={props.matchId}
           data-matched={props.matched}
           className={`
-          ${props.color ? " bg-mc-g  dark:bg-mc-g " : "dark:bg-zinc-700  dark:text-bl-1"}
+          ${props.color ? " bg-mc-g  dark:bg-mc-g " : "dark:bg-zinc-700  bg-bl-2  dark:text-bl-1"}
           card  h-16 w-64     
           rounded-xl shadow-md  flex justify-center items-center text-bd-1  `}
         >

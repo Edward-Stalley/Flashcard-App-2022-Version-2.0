338c1ce590f99eb11b921b6a9fe27672c0cf5b8b
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-14 20:17:36 +0900

    Fixed <Head>

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MyThemeContextProvider } from "../store/myThemeContext";
-import { Head } from "next/document";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

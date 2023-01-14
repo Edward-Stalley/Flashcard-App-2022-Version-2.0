2aee176e8307a6ff24842fdb4e55581dd34626d3
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-14 21:20:06 +0900

    hardcoded db (need to change to .env)

--- client/pages/api/ClassSelector/[...id].ts
+++ client/pages/api/ClassSelector/[...id].ts
@@ -4,11 +4,18 @@ import mysql from "mysql";
 
 // Hosted Online
 
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "db4free.net",
+  user: "thelazyboon",
+  password: "spirited",
+  database: "flashcards",
 });
 
 export default function getClass(req: NextApiRequest, res: NextApiResponse) {

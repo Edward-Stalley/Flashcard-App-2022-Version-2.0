82ee5304b396d15ac270ebf5f8893b27a0e25e50
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-23 17:49:13 +0900

    Try with process.env

--- client/next.config.js
+++ client/next.config.js
@@ -5,11 +5,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  // env: {
+  //   DB_HOST: "db4free.net",
+  //   DB_USER: "thelazyboon",
+  //   DB_PASSWORD: "spirited",
+  //   DB_DATABASE: "flashcards",
+  // },
+
   env: {
-    DB_HOST: "db4free.net",
-    DB_USER: "thelazyboon",
-    DB_PASSWORD: "spirited",
-    DB_DATABASE: "flashcards",
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_DATABASE: process.env.DB_DATABASE,
   },
 };
 

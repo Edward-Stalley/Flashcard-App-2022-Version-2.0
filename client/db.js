462bf11f18c862879c3a3cc3a0b981a308b1736c
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-24 17:27:28 +0900

    git ignore

--- client/db.js
+++ client/db.js
@@ -1,7 +1,6 @@
 // import mysql from "mysql";
-// need to install this dependency
+
 import mysql from "serverless-mysql";
-// const mysql = require("serverless-mysql")();
 
 const db = mysql({
   config: {
@@ -21,10 +20,3 @@ export default async function executeQuery({ query }) {
     return { error };
   }
 }
-
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-// });
--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// i commented this out to test something but it is usally on so revert back if there are problems
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,

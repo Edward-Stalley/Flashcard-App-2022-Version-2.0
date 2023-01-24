be8d18465871dc47437d451a98872e1968eb0aac
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-01-24 11:48:17 +0900

    Update next.config

--- client/next.config.js
+++ client/next.config.js
@@ -5,19 +5,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  // env: {
-  //   DB_HOST: "db4free.net",
-  //   DB_USER: "thelazyboon",
-  //   DB_PASSWORD: "spirited",
-  //   DB_DATABASE: "flashcards",
-  // },
-
   env: {
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_DATABASE: process.env.DB_DATABASE,
+    DB_HOST: "db4free.net",
+    DB_USER: "thelazyboon",
+    DB_PASSWORD: "spirited",
+    DB_DATABASE: "flashcards",
   },
+
+  // env: {
+  //   DB_HOST: process.env.DB_HOST,
+  //   DB_USER: process.env.DB_USER,
+  //   DB_PASSWORD: process.env.DB_PASSWORD,
+  //   DB_DATABASE: process.env.DB_DATABASE,
+  // },
 };
 
 module.exports = nextConfig;

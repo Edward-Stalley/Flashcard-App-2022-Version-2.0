083ae5ee26335a9a5833a1806158f4554e386228
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-02-02 16:32:40 +0900

    Git Ignore Update

---
+++ client/next.config.js
@@ -0,0 +1,42 @@
+/** @type {import('next').NextConfig} */
+require("dotenv").config();
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  env: {
+    DB_HOST: "englishbuffetinstance.cmargdrwu7dt.ap-northeast-1.rds.amazonaws.com",
+    DB_USER: "admin",
+    DB_PASSWORD: "ddHgks63018daj23",
+    DB_DATABASE: "EB_flashcards_DB",
+
+    // DB_HOST: process.env.DB_HOST,
+    // DB_PASSWORD: process.env.DB_PASSWORD,
+    // DB_USER: process.env.DB_USER,
+    // DB_DATABASE: process.env.DB_DATABASE,
+
+    // DB_HOST: "db4free.net",
+    // DB_USER: "thelazyboon",
+    // DB_PASSWORD: "spirited",
+    // DB_DATABASE: "flashcards",
+  },
+
+  // env: {
+  //   DB_HOST: process.env.DB_HOST,
+  //   DB_USER: process.env.DB_USER,
+  //   DB_PASSWORD: process.env.DB_PASSWORD,
+  //   DB_DATABASE: process.env.DB_DATABASE,
+  // },
+};
+
+module.exports = nextConfig;
+
+// export async function rewrites() {
+//   return [
+//     {
+//       source: "sql6.freemysqlhosting.net",
+//       destination: "http://localhost:8800/:path*",
+//     },
+//   ];
+// }

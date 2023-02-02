5d2ce902721f8c12dfe841dfe1e47a708bedc72a
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2023-02-02 17:13:58 +0900

    Year Selector CSS

--- client/Components/YearComponent.tsx
+++ client/Components/YearComponent.tsx
@@ -7,7 +7,7 @@ export default function Year(props: any) {
 
   return (
     <Link
-      className="hover:bg-blue-300/75 bg-blue-200 dark:hover:bg-zinc-700 dark:bg-bd-1 dark:text-td-1   w-56 h-16 p-5 rounded flex items-center m-1 justify-center"
+      className="nav_button bg-but-d dark:text-bd-1  w-56 h-16  p-5 rounded-full flex items-center m-1 justify-center"
       href={`ClassSelector/${props.year}`}
     >
       Year {firstYearHalf} - {secondYearHalf}
--- client/pages/ClassSelector/index.tsx
+++ client/pages/ClassSelector/index.tsx
@@ -3,6 +3,7 @@ import HomeButton from "../../Components/HomeButton";
 import YearComponent from "../../Components/YearComponent";
 import ToggleButton from "../../Components/ToggleButton";
 import Header from "../../Components/Header";
+import Navbar from "../../Components/Navbar";
 
 export default function Product() {
   const yearArray = ["2022", "2023"];
@@ -12,14 +13,24 @@ export default function Product() {
   });
 
   return (
-    <div>
-      <div>
-        <Header pageHeader="Select The Year" subHeader={""} />
-        <div className="flex items-center  justify-between bg-blue-200 dark:bg-bd-1   "></div>
-        <div className="bg-blue-200  min-h-screen  items-center p-5 flex-col flex dark:bg-bd-1  ">
-          <div className="bg-blue-100 dark:bg-bd-2 rounded  p-5 grid items-center justify-center ">{yearList}</div>
+    <>
+      <div className="dark:bg-gray-800  bg-bl-1 text-bd-1 dark:text-bl-1 grid  ">
+        <div>
+          <Navbar />
+          <Header pageHeader="Select Year" subHeader={""} />
+        </div>
+        <div>
+          <div className=" min-h-screen  items-center p-5 flex-col flex dark:bg-bd-1  ">
+            <div className=" dark:bg-bd-1 rounded  p-5 gap-2 grid mobile:grid-cols-1 sm:grid-cols-1  md:grid-cols-1 xl:grid-cols-1 items-center justify-center ">
+              {yearList}
+            </div>
+          </div>
+          {/* <div className="flex items-center  justify-between bg-blue-200 dark:bg-bd-1   "></div>
+          <div className="bg-blue-200  min-h-screen  items-center p-5 flex-col flex dark:bg-bd-1  ">
+            <div className="bg-blue-100 dark:bg-bd-2 rounded  p-5 grid items-center justify-center ">{yearList}</div>
+          </div> */}
         </div>
       </div>
-    </div>
+    </>
   );
 }
